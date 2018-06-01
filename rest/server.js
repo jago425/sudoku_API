7e040e9e50ea31e7faec58a1b6a2fcc891e45239
Rachel Jagodowski% <Rachel.Jagodowski@Libertymutual.com%>
2018-06-01 08:54:16 -0400

    initial commit

---
+++ rest/server.js
@@ -0,0 +1,11 @@
+var express     = require('express');
+var bodyParser  = require('body-parser');
+
+var app         = express(); // Please do not remove this line, since CLI uses this line as guidance to import new controllers
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running');
+});
